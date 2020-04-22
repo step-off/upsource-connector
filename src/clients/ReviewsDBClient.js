9cd0e6b1b91e228d697613f3a43382277cb75b7b
step-off <nofantasy12@gmail.com>
2020-04-22 19:53:59 +0500

    feat(db): add `notify_chats` collection

--- src/clients/ReviewsDBClient.js
+++ src/clients/ReviewsDBClient.js
@@ -7,20 +7,27 @@ class ReviewsDBClient {
       useUnifiedTopology: true,
     });
     this.dbName = 'xcom_reviews';
+    this._openedReviewsCollectionName = 'xcom_opened_reviews';
+    this._reviewsUsersCollectionName = 'xcom_reviews_users';
+    this._notifyChatsCollectionName = 'notify_chats';
     this._reviewsUsersCache = null;
     this._isConnected = false;
     this._setCleanupHandler();
+    this._connectionPromise = null;
+    this._close = this._close.bind(this);
+    this._connect();
   }
 
   async getOpenedReviews() {
-    try {
-      await this._connect();
-    } catch (e) {
-      console.error(`Unable to connect to database:`, e);
+    if (this._connectionPromise) {
+      await this._connectionPromise;
+    }
+    if (!this._isConnected) {
+      console.error(`Try to get opened reviews while there is no connection`);
       return [];
     }
     const db = this.client.db(this.dbName);
-    const reviewsCollection = db.collection('xcom_opened_reviews');
+    const reviewsCollection = db.collection(this._openedReviewsCollectionName);
     return reviewsCollection.find({}).toArray();
   }
 
@@ -28,16 +35,52 @@ class ReviewsDBClient {
     if (this._reviewsUsersCache && this._reviewsUsersCache.length > 0) {
       return this._reviewsUsersCache;
     }
-    try {
-      await this._connect();
-    } catch (e) {
-      console.error(`Unable to connect to database:`, e);
+    if (this._connectionPromise) {
+      await this._connectionPromise;
+    }
+    if (!this._isConnected) {
+      console.error(`Try to get reviews users while there is no connection`);
       return [];
     }
+
     const db = this.client.db(this.dbName);
-    const usersCollection = db.collection('xcom_reviews_users');
+    const usersCollection = db.collection(this._reviewsUsersCollectionName);
     this._reviewsUsersCache = usersCollection.find({}).toArray();
     return this._reviewsUsersCache;
+  }
+
+  async getChatsToNotify() {
+    if (this._connectionPromise) {
+      await this._connectionPromise;
+    }
+    if (!this._isConnected) {
+      console.error(`Try to get chats while there is no connection`);
+      return [];
+    }
+
+    const db = this.client.db(this.dbName);
+    const notifyChatsCollection = db.collection(this._notifyChatsCollectionName);
+    return notifyChatsCollection.find({}).toArray();
+  }
+
+  async insertNewChatsToNotify(chats) {
+    if (this._isConnected && chats.length > 0) {
+      const db = this.client.db(this.dbName);
+      const chatsCollection = db.collection(this._notifyChatsCollectionName);
+      const chatsInCollection = await chatsCollection.find({}).toArray();
+      const existingChatsIds = chatsInCollection.map((i) => i.id);
+      const newChats = chats.filter((i) => !existingChatsIds.includes(i.id));
+
+      if (newChats.length === 0) {
+        return;
+      }
+
+      try {
+        await chatsCollection.insertMany(newChats);
+      } catch (e) {
+        console.error(`Unable to insert reviews: ${newChats}`, e);
+      }
+    }
   }
 
   async removeObsoleteReviews(reviews) {
@@ -49,7 +92,7 @@ class ReviewsDBClient {
     }
 
     const db = this.client.db(this.dbName);
-    const reviewsCollection = db.collection('xcom_opened_reviews');
+    const reviewsCollection = db.collection(this._openedReviewsCollectionName);
     try {
       await reviewsCollection.deleteMany({ _id: { $in: reviews.map((i) => i._id) } });
     } catch (e) {
@@ -65,7 +108,7 @@ class ReviewsDBClient {
       console.error('Try to insert reviews while connection is closed.');
     }
     const db = this.client.db(this.dbName);
-    const reviewsCollection = db.collection('xcom_opened_reviews');
+    const reviewsCollection = db.collection(this._openedReviewsCollectionName);
 
     try {
       await reviewsCollection.insertMany(reviews);
@@ -79,11 +122,18 @@ class ReviewsDBClient {
       return;
     }
     try {
-      await this.client.connect();
+      console.log(`Connecting to database...`);
+
+      this._connectionPromise = this.client.connect();
+      await this._connectionPromise;
       this._isConnected = true;
+
+      console.log(`Connected to database successfully!`);
     } catch (e) {
       console.log(`Could not to connect to database`);
       console.log(e);
+    } finally {
+      this._connectionPromise = null;
     }
   }
 
