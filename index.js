91f4d7afac57c0c150606827a8ffd117291f0bcb
step-off <nofantasy12@gmail.com>
2020-04-26 09:52:29 +0500

    refactor(service): respond 200 OK to any request

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const http = require('http');
 const worker = require('./src/worker');
 
 const defaultPort = 8000;
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.write('OK');
+  res.end();
+});
 
 server.listen(process.env.PORT || defaultPort, worker);
